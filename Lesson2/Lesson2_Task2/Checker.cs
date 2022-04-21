32d24455cbdb98c6c72a8396d284ae48613002bd
efess29 <efess29@gmail.com>
2022-04-21 18:28:04 +0300

    add task 2

---
+++ Lesson2/Lesson2_Task2/Checker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_Task2
+{
+    public class Checker
+    {
+        public Checker()
+        {
+        }
+
+        public int InputA { get; set; }
+
+        public int InputB { get; set; }
+
+        public decimal ExpectedValue { get; set; }
+
+        public string ExpectedValueString { get; set; }
+    }
+}
