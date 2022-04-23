ed079baa2291bcbc33c2c6e127e250b818af43d8
efess29 <efess29@gmail.com>
2022-04-23 18:08:22 +0300

    add lesson3 task

---
+++ Lesson3/Lesson3/PointClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson3
+{
+    internal class PointClass<T> : ICloneable
+    {
+        public PointClass(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public T X { get; set; }
+        
+        public T Y { get; set; }
+
+        public object Clone() => new PointClass<T>(X, Y);
+    }
+
+    internal struct PointStruct<T> : ICloneable
+    {
+        public PointStruct(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public T X { get; set; }
+
+        public T Y { get; set; }
+
+        public object Clone() => new PointStruct<T>(X, Y);
+    }
+}
---
+++ Lesson3/Lesson3/Size.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson3
+{
+    public class Size<T> : ICloneable
+    {
+        public Size(T height, T width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public T Height { get; }
+        
+        public T Width { get; }
+
+        public object Clone() => new Size<T>(Height, Width);
+    }
+}
