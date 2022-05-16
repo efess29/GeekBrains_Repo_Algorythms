6faa2c738fc6b862d5ead2b6ba7e945a8d5cb0fe
efess29 <efess29@gmail.com>
2022-05-16 16:20:46 +0300

    add lesson 6 task

---
+++ Lesson6/Lesson6_Task1/MainGraph.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson6_Task1
+{
+    public class MainGraph
+    {
+        List<Vertex> Vertexes = new List<Vertex>();
+        List<Edge> Edges = new List<Edge>();
+
+        public int VertexCount => Vertexes.Count;
+        
+        public int EdgesCount => Edges.Count;
+
+        /// <summary>
+        /// Представляет метод добавления вершин в коллекцию
+        /// </summary>
+        /// <param name="vertex"></param>
+        public void AddVertex(Vertex vertex)
+        {
+            Vertexes.Add(vertex);
+        }
+
+        /// <summary>
+        /// Представляет метод добавления ребер в коллекцию
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            Edges.Add(edge);
+        }
+
+        /// <summary>
+        /// Представляет метод поиска графа в глубину
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="searchValue"></param>
+        /// <returns></returns>
+        public List<Vertex> DFSSearch(Vertex start, int searchValue)
+        {
+            Console.WriteLine();
+            Console.WriteLine("DFS Search");
+            Console.WriteLine();
+
+            var buffer = new Stack<Vertex>();
+            List<Vertex> returnArray = new List<Vertex>();
+
+            buffer.Push(start);
+
+            if (buffer == null)
+            {
+                return null;
+            }
+
+            while (buffer.Count != 0)
+            {
+                var element = buffer.Pop();
+
+                if (element.Number == searchValue)
+                {
+                    element.Visited = true;
+                    Console.WriteLine();
+                    Console.WriteLine($"Required element: [{element.Number}] Visited: [{element.Visited}] ");
+                    break;
+                }
+
+                returnArray.Add(element);
+                element.Visited = true;
+                
+                Console.WriteLine($"--> [{element.Number}] Visited: [{element.Visited}]");
+
+                for (int i = 0; i < Edges.Count; i++)
+                {
+                    if (element == Edges[i].From)
+                    {
+                        if (Edges[i].To.Visited != true)
+                        {
+                            buffer.Push(Edges[i].To);
+                        }
+                    }
+                }
+            }
+
+            return returnArray;
+        }
+
+        /// <summary>
+        /// Представляет волновой метод поиска графа
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="searchValue"></param>
+        /// <returns></returns>
+        public List<Vertex> BFSSearch(Vertex start, int searchValue)
+        {
+            Console.WriteLine();
+            Console.WriteLine("BFS Search");
+            Console.WriteLine();
+
+            var buffer = new Queue<Vertex>();
+            var returnArray = new List<Vertex>();
+            buffer.Enqueue(start);
+
+            if (buffer == null)
+            {
+                return null;
+            }
+
+            while (buffer.Count != 0)
+            {
+                var element = buffer.Dequeue();
+
+                if (element.Number == searchValue)
+                {
+                    element.Visited = true;
+                    Console.WriteLine();
+                    Console.WriteLine($"Required element: [{element.Number}] Visited: [{element.Visited}] ");
+                    break;
+                }
+
+                else
+                {
+                    foreach (var edgesNear in Edges)
+                    {
+                        if (element == edgesNear.From)
+                        {
+                            buffer.Enqueue(edgesNear.To);
+                        }
+                    }
+
+                    if (element.Visited != true)
+                    {
+                        returnArray.Add(element);
+                        element.Visited = true;
+                        Console.WriteLine($"--> [{element.Number}] [{element.Visited}]");
+                    }
+                }
+            }
+
+            return returnArray;
+        }
+
+        /// <summary>
+        /// Представляет метод вывода списка смежностей
+        /// </summary>
+        /// <param name="vertex"></param>
+        public void PrintVertex(Vertex vertex)
+        {
+            Console.Write($"[{vertex.Number}] -> ");
+            
+            foreach (var v in GetVertex(vertex))
+            {
+                Console.Write($"[{v}];");
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Представляет метод получения вершины
+        /// </summary>
+        /// <param name="vertex"></param>
+        /// <returns></returns>
+        private HashSet<Vertex> GetVertex(Vertex vertex)
+        {
+            var result = new HashSet<Vertex>();
+
+            foreach (var edge in Edges)
+            {
+                if (vertex == edge.From)
+                {
+                    result.Add(edge.To);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Представляет метод вывода матрицы смежности
+        /// </summary>
+        /// <param name="graph"></param>
+        public void PrintMatrix(MainGraph graph)
+        {
+            Console.WriteLine("Adjacency matrix");
+            Console.WriteLine();
+
+            var matrix = graph.GetMatrix();
+            
+            for (int i = 0; i < graph.VertexCount; i++)
+            {
+                Console.Write($"{i + 1} |");
+                
+                for (int j = 0; j < graph.VertexCount; j++)
+                {
+                    Console.Write(string.Format("{0, 3}", $"{matrix[i, j]}"));
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Представляет метод вывода матрицы смежности
+        /// </summary>
+        /// <returns></returns>
+        private int[,] GetMatrix()
+        {
+            var matrix = new int[Vertexes.Count, Vertexes.Count];
+
+            foreach (var edge in Edges)
+            {
+                var row = edge.From.Number - 1;
+                var column = edge.To.Number - 1;
+
+                matrix[row, column] = edge.Weight;
+            }
+            return matrix;
+        }
+    }
+}
---
+++ Lesson6/Lesson6_Task1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Lesson6_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graph = new MainGraph();
+
+            var v1 = new Vertex(1);
+            var v2 = new Vertex(2);
+            var v3 = new Vertex(3);
+            var v4 = new Vertex(4);
+            var v5 = new Vertex(5);
+            var v6 = new Vertex(6);
+            var v7 = new Vertex(7);
+            var v8 = new Vertex(8);
+
+            graph.AddVertex(v1);
+            graph.AddVertex(v2);
+            graph.AddVertex(v3);
+            graph.AddVertex(v4);
+            graph.AddVertex(v5);
+            graph.AddVertex(v6);
+            graph.AddVertex(v7);
+            graph.AddVertex(v8);
+
+            graph.AddEdge(v1, v2);
+            graph.AddEdge(v2, v3);
+            graph.AddEdge(v2, v4);
+            graph.AddEdge(v4, v5);
+            graph.AddEdge(v1, v6);
+            graph.AddEdge(v6, v7);
+            graph.AddEdge(v6, v8);
+
+            graph.PrintMatrix(graph);
+            
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine();
+            Console.WriteLine("Adjacency list");
+            Console.WriteLine();
+
+            graph.PrintVertex(v1);
+            graph.PrintVertex(v2);
+            graph.PrintVertex(v3);
+            graph.PrintVertex(v4);
+            graph.PrintVertex(v5);
+            graph.PrintVertex(v6);
+            graph.PrintVertex(v7);
+            graph.PrintVertex(v8);
+
+            Console.ReadKey();
+            Console.Clear();
+
+            // ========= TEST 1 ========= //
+            var checker = new Checker[4];
+            checker[0] = new Checker()
+            {
+                InputA = 8,
+                InputB = v1,
+                ExpectedValue = 8,
+            };
+
+            checker[1] = new Checker()
+            {
+                InputA = 4,
+                InputB = v2,
+                ExpectedValue = 4,
+            };
+
+            checker[2] = new Checker()
+            {
+                InputA = 2,
+                InputB = v1,
+                ExpectedValue = 2,
+            };
+
+            checker[3] = new Checker()
+            {
+                InputA = 5,
+                InputB = v2,
+                ExpectedValue = 5,
+            };
+
+            // ========= TEST BFS ========= //
+
+            for (int i = 0; i < checker.Length; i++)
+            {
+                Console.WriteLine($"Element search starts from: [{checker[i].InputB}] - Expecting: [{checker[i].InputA}]");
+                
+                graph.BFSSearch(checker[i].InputB, checker[i].InputA);
+                
+                Console.ReadKey();
+                Console.Clear();
+
+                v1.Visited = false;
+                v2.Visited = false;
+                v3.Visited = false;
+                v4.Visited = false;
+                v5.Visited = false;
+                v6.Visited = false;
+                v7.Visited = false;
+                v8.Visited = false;
+            }
+
+            // ========= TEST DFS ========= //
+
+            for (int i = 0; i < checker.Length; i++)
+            {
+                Console.WriteLine($"Element search starts from: [{checker[i].InputB}] - Expecting: [{checker[i].InputA}]");
+                
+                graph.DFSSearch(checker[i].InputB, checker[i].InputA);
+                
+                Console.ReadKey();
+                Console.Clear();
+
+                v1.Visited = false;
+                v2.Visited = false;
+                v3.Visited = false;
+                v4.Visited = false;
+                v5.Visited = false;
+                v6.Visited = false;
+                v7.Visited = false;
+                v8.Visited = false;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
