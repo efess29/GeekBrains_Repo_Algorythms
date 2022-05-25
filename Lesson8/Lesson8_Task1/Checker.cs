d67585cc5df011edbbbe5fa0e41b87f49f4a111d
efess29 <efess29@gmail.com>
2022-05-26 00:17:10 +0300

    add task 1 / 2

---
+++ Lesson8/Lesson8_Task1/Checker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson8_Task1
+{
+    public class Checker
+    {
+        public int InputA { get; set; }
+
+        public int InputB { get; set; }
+
+        public string ExpectedString { get; set; }
+
+        public int ExpectedValue { get; set; }
+    }
+}
